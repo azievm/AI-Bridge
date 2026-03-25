9759b6e1699fff979ef6cbf2913340b3206d4f50
Majid Aziev <majidaziev007@yandex.ru>
2026-03-26 02:00:38 +0300

    Gemini API connect

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from gemini_client import get_answer_from_gemini
 
 app = FastAPI()
 
 @app.get("/requests")
 def get_my_requests():
     return "Hello world"
+
+
+@app.post("/requests")
+def send_my_prompt(
+    prompt: str
+):
+    answer = get_answer_from_gemini(prompt)
+
+    return {"answer": answer}
